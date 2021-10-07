2496352a7ebed47666b9778b594225ec73f6f568
Steve Smith <ssmith@atlassian.com>
2021-10-07 19:25:03 +1100

    BAMBK8S-84: Remove old file settings that are no longer used.

--- probe-common.sh
+++ probe-common.sh
@@ -1,5 +1,4 @@
 
 WRAPPER_STATUSFILE="/var/atlassian/application-data/bamboo-agent/bin/bamboo-agent.status"
 JAVA_STATUSFILE="/var/atlassian/application-data/bamboo-agent/bin/bamboo-agent.java.status"
-AGENT_LOG="/var/atlassian/application-data/bamboo-agent/logs/atlassian-bamboo.log"
-LOG_TARGET="Bamboo agent '${HOSTNAME}' ready to receive builds."
+
