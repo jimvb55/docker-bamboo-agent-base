63c40aa1430795bcce12991b1bdef3bca7546f22
Steve Smith <ssmith@atlassian.com>
2021-10-21 10:52:44 +1100

    BAMBK8S-86: Add probe tests.

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 
 from helpers import get_app_home, get_app_install_dir, get_bootstrap_proc, get_procs, \
     parse_properties, parse_xml, run_image, wait_for_http_response, wait_for_proc
@@ -27,3 +28,35 @@ def test_wrapper_conf(docker_cli, image, run_user):
     assert f'-Xmx{environment["WRAPPER_JAVA_MAXMEMORY"]}m' in jvm
     assert f'-Dbamboo.agent.ignoreServerCertName={environment["IGNORE_SERVER_CERT_NAME"]}' in jvm
     assert f'-Dbamboo.allow.empty.artifacts={environment["ALLOW_EMPTY_ARTIFACTS"]}' in jvm
+
+def test_startup_probe(docker_cli, image, run_user):
+    environment = {
+        'BAMBOO_SERVER': 'http://localhost',
+        'BAMBOO_AGENT_PERMISSIVE_READINESS': 'true',
+    }
+
+    container = docker_cli.containers.run(image, detach=True, user=run_user, environment=environment)
+
+    for i in range(10*60):
+        (exit_code, _output) = container.exec_run('/probe-startup.sh')
+        if exit_code == 0:
+            return
+        time.sleep(0.1)
+
+    assert False
+
+def test_readiness_probe(docker_cli, image, run_user):
+    environment = {
+        'BAMBOO_SERVER': 'http://localhost',
+        'BAMBOO_AGENT_PERMISSIVE_READINESS': 'true',
+    }
+
+    container = docker_cli.containers.run(image, detach=True, user=run_user, environment=environment)
+
+    for i in range(10*60):
+        (exit_code, _output) = container.exec_run('/probe-readiness.sh')
+        if exit_code == 0:
+            return
+        time.sleep(0.1)
+
+    assert False
